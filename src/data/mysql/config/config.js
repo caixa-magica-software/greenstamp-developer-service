eec2ff52b7baa78e0de04d2200476e09f0e1c2f7
rodrigo.resende <rodrigo.resende@caixamagica.pt>
2023-05-05 11:52:33 +0100

    Updated MySQL config to use only .env

--- src/data/mysql/config/config.js
+++ src/data/mysql/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    database: "developer-service-db",
-    username: "root",
-    password: "devpassword",
-    host: "172.20.0.2",
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    host: process.env.DATABASE_URI,
     dialect: "mysql",
     define: {
       timestamps: false,
