29658b6b24a5164733cf8dc5519b304d4db490ab
Adriano Campos <adrianoribeirocampos@gmail.com>
2023-05-05 20:13:12 +0100

    Add mysql PORT configuration

--- src/routes/get-all.js
+++ src/routes/get-all.js
@@ -7,12 +7,14 @@ const host = process.env.DATABASE_URI;
 const user = process.env.DATABASE_USER;
 const password = process.env.DATABASE_PASS;
 const database = process.env.DATABASE_NAME;
+const port = process.env.DATABASE_PORT;
 
 const db = mysql.createConnection({
   host,
   user,
   password,
   database,
+  port,
 });
 
 router.get("/", (req, res) => {
