73848564103c7843d85615de874602f7c7e7a4e9
Adriano Campos <adrianoribeirocampos@gmail.com>
2023-06-19 16:40:39 +0100

    Add api to get aptoide api data

--- src/routes/get-api-aptoide.js
+++ src/routes/get-api-aptoide.js
@@ -1,39 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const url = require('url');
 
 const { default: axios } = require("axios");
 
 router.get("/", async (req, res) => {
   console.log("req.url: " + req.url);  
-  const apiUrl = req.url.replace('/', '');
-  const fullUrl = `https://ws75.aptoide.com/api/7/getApp${apiUrl}`;
+  const getApplicationInfo = (packageName) => {
+    return new Promise((resolve, reject) => {
+      const params = { package_name: packageName }
+      axios.get(`${process.env.APTOIDE_API_BASE_URL}/getApp`, { params: params})
+        .then(response => resolve({code: 200, data: response.data}))
+        .catch(error => parseError(error, reject))
+    })
+  }
 
-  return new Promise((resolve, reject) => {
-    const params = { package_name: "com.valvesoftware.android.steam.community", vercode: "8116642" }
-    axios.get(`${process.env.APTOIDE_API_BASE_URL}/getApp`, { params: params})
-      .then(
-        console.log(response)
-      )
-      .catch(error => parseError(error, reject))
-  })
-
-
-  /*console.log("fullurl: " + fullUrl);
   try {
-    const response = await axios({
-      method: req.method,
-      url: fullUrl,
-      data: req.body,
-      headers: req.headers,
-    });
+    const parsedUrl = url.parse(req.url, true);
+    const packageName = parsedUrl.query.package_name;
+    console.log(packageName)
 
-    res.send(response.data);
-    console.log("api res : " + response.data)
-  } catch (error) {
-  const statusCode = error.response ? error.response.status : 500;
-    console.log("api error: " + error)
-    res.status(statusCode).send(error.message);
-  } */
+    const appInfo = await getApplicationInfo(packageName)
+    //console.log((JSON.stringify(appInfo)))
+    return res.json(appInfo);
+  } catch(error) {
+    return res.status(401).json({error})
+  }
+
 
 });
 
