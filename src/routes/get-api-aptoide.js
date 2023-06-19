725c014a7e3294559c6bf582b919cc67af1170ba
Adriano Campos <adrianoribeirocampos@gmail.com>
2023-06-19 14:49:50 +0100

    Api proxy to api aptoide

--- src/routes/get-api-aptoide.js
+++ src/routes/get-api-aptoide.js
@@ -7,7 +7,18 @@ router.get("/", async (req, res) => {
   console.log("req.url: " + req.url);  
   const apiUrl = req.url.replace('/', '');
   const fullUrl = `https://ws75.aptoide.com/api/7/getApp${apiUrl}`;
-  console.log("fullurl: " + fullUrl);
+
+  return new Promise((resolve, reject) => {
+    const params = { package_name: "com.valvesoftware.android.steam.community", vercode: "8116642" }
+    axios.get(`${process.env.APTOIDE_API_BASE_URL}/getApp`, { params: params})
+      .then(
+        console.log(response)
+      )
+      .catch(error => parseError(error, reject))
+  })
+
+
+  /*console.log("fullurl: " + fullUrl);
   try {
     const response = await axios({
       method: req.method,
@@ -22,7 +33,7 @@ router.get("/", async (req, res) => {
   const statusCode = error.response ? error.response.status : 500;
     console.log("api error: " + error)
     res.status(statusCode).send(error.message);
-  }
+  } */
 
 });
 
