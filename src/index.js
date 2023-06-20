03850943e430abba1232450edb9b0995b90751df
Adriano Campos <adrianoribeirocampos@gmail.com>
2023-06-20 17:33:47 +0100

    Return data ranking for each analyser

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ app.get('/_/health', (req, res) => res.sendStatus(200));
 app.use('/api/analyze', require('./routes/analyze'))
 app.use('/api/result', require('./routes/result'))
 app.use('/api/get-all', require('./routes/get-all'))
-app.use('/api/get-all-formatted', require('./routes/get-all-formatted'))
+// app.use('/api/get-all-formatted', require('./routes/get-all-formatted'))
+app.use('/api/get-all-formatted-wcec', require('./routes/get-all-formatted-wcec'))
+app.use('/api/get-all-formatted-kadabra', require('./routes/get-all-formatted-kadabra'))
 app.use('/api/get-api-aptoide', require('./routes/get-api-aptoide'))
 
 app.listen(process.env.PORT || 3000, () => {
