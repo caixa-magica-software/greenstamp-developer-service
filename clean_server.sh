b6f4e6c9cca0bee2029eaba77fb1e8ac31bbc5b9
Adriano Campos <adrianoribeirocampos@gmail.com>
2023-07-17 17:12:20 +0100

    Add script to screan analysers temp files

---
+++ clean_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clear wcec directory
+sudo rm -rf /data/greenstamp/wcec/*
+
+# Clear analyzer-earmo-api uploads directory
+sudo rm -rf /data/greenstamp/analyzer-earmo-api/uploads/*
+
+# Clear analyzer-earmo-api earmo_bin directory
+sudo rm -rf /data/greenstamp/analyzer-earmo-api/earmo_bin/*
+
+# Clear analyzer-wcec-api directory
+sudo rm -rf /data/greenstamp/analyzer-wcec-api/*
+
+# Clear analyzer-kadabra-api uploads directory
+sudo rm -rf /data/greenstamp/analyzer-kadabra-api/uploads/*
